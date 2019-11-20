9103cabbc2284358adf41e4d46d7c6e8439ac3a8
lumo <932262558@qq.com>
2019-11-20 08:09:09 +0800

    提交

--- dubbo/dubbo_first/first_dubbo_server/first_dubbo_server_provider/src/main/java/com/lumor/dubbo/LoginService.java
+++ dubbo/dubbo_first/first_dubbo_server/first_dubbo_server_provider/src/main/java/com/lumor/dubbo/LoginService.java
@@ -3,6 +3,6 @@ package com.lumor.dubbo;
 public class LoginService implements ILoginService {
     @Override
     public String login(String username) {
-        return username + "login success";
+        return username + ": login success";
     }
 }
